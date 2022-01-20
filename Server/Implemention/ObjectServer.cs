0d2d981343e8873d8b4e732d2e5a9e5fdf68d5e2
HadarCohen1 <98028760+HadarCohen1@users.noreply.github.com>
2022-01-20 14:32:32 +0200

    ObjectServer

---
+++ Server/Implemention/ObjectServer.cs
@@ -0,0 +1,26 @@
+﻿using PingPong.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Implemention
+{
+    class ObjectServer<T> : IServer<T>
+    {
+        public ISocket<T> Socket { get; set; }
+        public object ToCreate;
+
+        public ObjectServer(ISocket<T> socket, object toCreate)
+        {
+            Socket = socket;
+            ToCreate = toCreate;
+        }
+
+        public void StartListening()
+        {
+            Socket.StartListening();
+        }
+    }
+}
